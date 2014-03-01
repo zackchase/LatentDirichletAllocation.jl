1c28dd3c67f45a8163e8831fad9571ce73c2aa5c
Joseph Perla <joseph@ not an email.com>
2014-02-28 20:58:00 -0500

    make util tests pass

--- test/test_util.jl
+++ test/test_util.jl
@@ -1,4 +1,13 @@
 using Base.Test
+using Util
+
+function count(s)
+    counts = Int[0, 0, 0, 0]
+    for s in samples
+        counts[s] += 1
+    end
+    return counts
+end
 
 samples = [sample(Float64[3, 3, 3, 3]) for i in 1:100]
 println(count(samples))
