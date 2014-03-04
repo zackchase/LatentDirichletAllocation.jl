42131d3519c325b923bccbc898586c02af5455ea
Joseph Perla <joseph@ not an email.com>
2014-03-04 01:16:04 -0500

    Generate plots script

---
+++ generate_plots.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+julia plot.jl output/classic/b01k3i10000 data/classic/truelabels.txt
+julia plot.jl output/classic/b001k3i10000 data/classic/truelabels.txt
+julia plot.jl output/classic/b0001k3i10000 data/classic/truelabels.txt
+julia plot.jl output/classic/b00001k3i10000 data/classic/truelabels.txt
+julia plot.jl output/classic/b000001k3i10000 data/classic/truelabels.txt
